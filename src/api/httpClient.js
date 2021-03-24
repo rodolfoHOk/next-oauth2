e4fffa5cbfc70d037ef3d59cb6647ddb8a1e36be
rodolfoHOk <rodolfo.okino@gmail.com>
2021-03-24 20:25:15 -0300

    add .env.local.example

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-export const clientId = 'my-angular-app';
-
-export const clientSecret = '@321';
+export const clientId = process.env.clientId;
+export const clientSecret = process.env.clientSecret;
 
 const httpClient = axios.create({
   baseURL: 'http://localhost:8080'
